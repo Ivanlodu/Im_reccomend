175bc55deeb6c98dca80f4c9baa478078f1e8ef7
Ivanlodu <109980952+Ivanlodu@users.noreply.github.com>
2026-07-05 22:11:16 -0400

    user table

---
+++ backend/db/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+from datetime import datetime
+
+class user(Base):
+    __tablename__ = "users"
+    id= Column(Integer, primary_key=True, index=True)
+    spotify_id = Column(String, nullable=False, unique=True)
+    display_name = Column(String)
+    email = Column(String)
+    access_token = Column(String)
+    refresh_token = Column(String)
+    token_expires_at = Column(Datetime)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    
